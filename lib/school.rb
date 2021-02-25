799b07599f3d936973630413ca9bbbc84fd84e61
Shane Rachal <imspammails31@gmail.com>
2021-02-24 18:38:13 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade_level)
+        hash = {}
+        if @roster[grade_level]
+            @roster[grade_level] << name         
+        end
+        if !@roster[grade_level]
+            hash[grade_level] = []
+            hash[grade_level] << name
+        end
+        @roster.merge!(hash)
+    end
+
+    def grade(grade_level)
+        @roster[grade_level]
+    end
+
+    def sort
+        @roster[9] = @roster[9].sort
+        @roster
+    end
+end
